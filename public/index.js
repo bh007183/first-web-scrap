c673114ac969b98ea318c02ebc51539662c44379
Ben Hopkins <bjhops17@gmail.com>
2021-05-07 07:10:34 -0700

    able to append response

--- public/index.js
+++ public/index.js
@@ -1,6 +1,10 @@
 
 
-$.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent("https://www.trilogyed.com/about/careers/") + '&callback=?', function(data){
-	console.log(data);
+$.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent("https://www.trilogyed.com/about/careers/") + '&callback=?', function(res){
+	$("#body").append(res.contents);
+	console.log(res)
+
 });
+
+
 
