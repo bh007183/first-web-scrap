08ee7ab7b1f4732cbf7ed033b3cadba74084268e
Ben Hopkins <bjhops17@gmail.com>
2021-05-08 07:51:22 -0700

    app working but want to switch it to all backend

--- public/index.js
+++ public/index.js
@@ -1,10 +1,36 @@
+localStorage.setItem("numJobs", 0);
 
-
-$.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent("https://www.trilogyed.com/about/careers/") + '&callback=?', function(res){
-	$("#body").append(res.contents);
-	console.log(res)
-
-});
-
-
+// setInterval( function () {
+	$.getJSON(
+		"http://www.whateverorigin.org/get?url=" +
+		  encodeURIComponent("https://www.trilogyed.com/about/careers/") +
+		  "&callback=?",
+		function (res) {
+		  $("#body").append(res.contents);
+		  let counter = 0;
+		  $.each($(".job-title"), function () {
+			let string = $(this).text();
+			if (string.includes("Teaching Assistant")) {
+			  counter++;
+			  console.log($(this).text());
+			}
+		  });
+		  if (counter > localStorage.getItem("numJobs")) {
+			$.ajax("/sendMessage", {
+			  method: "POST",
+			  data: {
+				former: localStorage.getItem("numJobs"),
+				new: counter,
+			  },
+			}).then((res) => {
+			  localStorage.setItem("numJobs", counter);
+			});
+		  } else {
+			localStorage.setItem("numJobs", counter);
+		  }
+		}
+	  );
+	  
+	
+// }, 1000 * 60 * 60 * 24)
 
--- server.js
+++ server.js
@@ -1,36 +1,32 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
+
 // const db = require("./models");
 // Sets up the Express App
 var PORT = process.env.PORT || 8080;
-
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// var corsOptions = {
-//   origin: 'https://www.trilogyed.com/about/careers'
-// }
-// // corsOptions
+
 app.use(cors());
-// Static directory
+
 app.use(express.static("public"));
-/////////////////////////////////
-// const placeholder = require("./routes/placeholder.js")
 
+app.post("/sendMessage", (req, res) => {
+  console.log(req.body)
+  client.messages
+      .create({body: 'Trilogy has new job listings for TA positions', from: '+17044198270', to: '+19072312406'})
+      .then(message => res.json(message.sid));
+})
 
-// Routes
-// =============================================================
-// app.use(placeholder)
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-
-// Change force: to true if it's cool for the site to remove database items.
-// db.sequelize.sync({ force: false}).then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT http://localhost:" + PORT);
   });
-// });
