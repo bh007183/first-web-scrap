01026fca42cfeb113f5bd7993a6e60eec3104212
Ben Hopkins <bjhops17@gmail.com>
2021-05-10 07:36:20 -0700

    final product

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const axios = require("axios");
 const cors = require("cors");
 require("dotenv").config();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8070;
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
@@ -12,38 +12,33 @@ let numberOfJobs = 0;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(cors());
-
 app.use(express.static("public"));
-
+setInterval(() => {
 axios
   .get("https://www.trilogyed.com/about/careers")
   .then(function (res) {
-    if (res.data.split("Teaching Assistant").length - 1 > numberOfJobs) {
-      app.post("/sendMessage", (req, res) => {
-        console.log(req.body);
-        client.messages
-          .create({
-            body: "Trilogy has new job listings for TA positions",
-            from: "+17044198270",
-            to: "+19072312406",
-          })
-          .then((message) => res.json(message.sid));
-          numberOfJobs = res.data.split("Teaching Assistant").length - 1
-      });
+    if (res.data.split("Teaching Assistant").length - 1 > numberOfJobs){
+
+      client.messages
+        .create({
+          body: "Trilogy has new job listings for TA positions",
+          from: "+17044198270",
+          to: "+19072312406",
+        })
+        .then(function (message) {
+          numberOfJobs = res.data.split("Teaching Assistant").length - 1;
+          res.json(message.sid);
+        });
+    }else{
+      numberOfJobs = res.data.split("Teaching Assistant").length - 1;
     }
   })
   .catch((err) => console.log(err));
-  console.log(numberOfJobs)
-
-// app.post("/sendMessage", (req, res) => {
-//   console.log(req.body)
-//   client.messages
-//       .create({body: 'Trilogy has new job listings for TA positions', from: '+17044198270', to: '+19072312406'})
-//       .then(message => res.json(message.sid));
-// })
+}, 10000);
 
 app.listen(PORT, function () {
   console.log("App listening on PORT http://localhost:" + PORT);
 });
+
+
